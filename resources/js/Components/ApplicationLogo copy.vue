f7fc9b4536cbb1b9082ed85b8cf6f23170afbccc
omar <omar.tamer982@gmail.com>
2025-04-17 12:22:52 +0300

    replace auth app logo

---
+++ resources/js/Components/ApplicationLogo copy.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="d-flex items-center group">
+        App Logo
+    </div>
+</template>
+
+
+<style scoped>
+.rotate-icon {
+    @apply rotate-180;
+}
+</style>
